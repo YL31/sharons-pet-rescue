88c694e40bfd26bb659190ab0372f7c39f21a77b
Yevonnie L <46805387+YL31@users.noreply.github.com>
2023-03-14 00:05:08 -0400

    Add functions to display pets when button clicked

--- js/script.js
+++ js/script.js
@@ -30,3 +30,25 @@ const clover = createPet("Clover", "rabbit");
 const baxter = createPet("Baxter", "hamster");
 const cleo = createPet("Cleo", "rat");
 const francine = createPet("Francine", "turtle");
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+
+const showPets = function (petArray) {
+  pets.innerHTML = "";
+  for (let pet of petArray) {
+    let status = "Ready to play!";
+    if (pet.isTired >= 7) {
+      status = "Sleeping.";
+    }
+    let li = document.createElement("li");
+    li.innerHTML = `<span class = "pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+    pets.append(li);
+  }
+};
+
+statusButton.addEventListener("click", function () {
+  showPets(allPets);
+});
